503587ff727e13f3924c62be09ef95e481dcd438
Wandemberg Gibaut <wander.gibaut@gmail.com>
2024-05-07 23:18:03 -0300

    changes to suit my needs

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# paper_renamer_gemini
-A script that renames pdfs containing scientific papers using gemini based on the text on their first page.
+# Paper Renamer Gemini
+
+Paper Renamer is a script that automatically renames PDF files based on their content. Using Google's Gemini, the script extracts the first author's surname, the year of publication, and title to generate a meaningful filename. The advantage of this approach is that it can handle arbitrarily formatted pdfs. 
+
+### Examples
+[[Zhao 2023] LLaMA_Beyond_English_An_Empirical_Study_on_Language_Capability_Transfer.pdf](https://arxiv.org/abs/2401.01055)
+
+[[Hong 2022] CogAgent_A_Visual_Language_Model_for_GUI_Agents.pdf](https://arxiv.org/abs/2312.08914)
+
+[[Gromov 2022] The_Unreasonable_Ineffectiveness_of_the_Deeper_Layers.pdf](https://arxiv.org/abs/2403.17887)
+## Installation
+Download repo:
+```
+git clone https://github.com/wandgibaut/paper_renamer_gemini/
+```
+Install relevant python modules:
+```
+pip install -r requirements.txt
+```
+## Usage
+set GOOGLE_API_KEY with your google api key in the environment variables.
+
+Pass the directory containing the script as argument and run:
+
+```
+python paper_renamer.py --dir /path/to/pdfs
+```
+
+The script will process PDFs in the current directory and rename them. It will also create and update a json file with papers already processed to ensure the same info isn't sent multiple times. 
+
+## Notes
+This repo is based on the work of [ksmith9](https://github.com/ksmith9/paper_renamer_gpt), from which I've made a [fork](https://github.com/wandgibaut/paper_renamer_gpt) and adapted the code to use Google's Gemini instead of OpenAI's GPT-4.
+
+### License
+
+[MIT License](https://choosealicense.com/licenses/mit/)
---
+++ paper_renamer.py
@@ -0,0 +1,105 @@
+import PyPDF2
+import os
+import json
+import google.generativeai as genai
+import argparse
+
+# please set you api key in your environment variables
+GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+# File to keep track of perviously renamed pdfs
+RENAMED_PDFS_FILE = "renamed_pdfs.json"
+
+# function to extract pdf contents until desired number of pages or character count is reached. 
+def extract_text_from_pdf(pdf_path, pages_to_extract=3, min_chars=1000):
+    """Extract text from the first few pages of a PDF or until min_chars is reached."""
+    with open(pdf_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        extracted_text = ""
+        for page_num in range(min(pages_to_extract, len(reader.pages))):
+            page_text = reader.pages[page_num].extract_text()
+            extracted_text += page_text
+            # Check if the cumulative extracted text has reached min_chars
+            if len(extracted_text) >= min_chars:
+                break
+        return extracted_text[:min_chars]  # Return only up to min_chars characters
+
+# function to send prompt to gpt-4
+def get_filename_from_openai(text):
+    system_msg = "You are an assistant that suggests PDF filenames based on content. Suggest a filename in the format where you identify the first author's surname, the year of publication, and the title of publication. The format should be: \n '[surname year] title_of_publication.pdf' \n Include no extraneous text. 'pdf' shouldn't be used anywhere except in the file extension. Responses should only be in the forms like: \n [Ramkumar 2023] Mascara1b_CRIRES.pdf \n [Troutman 2011] βPICTORIS_rovibrational.pdf \n If there is an issue finding any of the info mentioned, with no additional text simply return: \nERROR\n"
+    messages = system_msg + f"\n\nContent:\n{text}"
+
+    client = genai.GenerativeModel("gemini-1.0-pro")
+    chat = client.start_chat(history=[])
+
+    response = chat.send_message(messages)
+
+    new_filename_raw = response.text
+    return new_filename_raw
+
+# Creates file tracking json if one doesn't exist, otherwise loads it
+def load_renamed_pdfs():
+    if os.path.exists(RENAMED_PDFS_FILE):
+        with open(RENAMED_PDFS_FILE, 'r') as f:
+            return json.load(f)
+    else:
+        with open(RENAMED_PDFS_FILE, 'w') as f:
+            json.dump([], f)  # Creating an empty JSON array
+        return []
+
+def save_renamed_pdfs(renamed_list):
+    with open(RENAMED_PDFS_FILE, 'w') as f:
+        json.dump(renamed_list, f)
+
+def main(paper_directory : str ='./data'):
+    directory = paper_directory
+    
+    renamed_pdfs = load_renamed_pdfs()
+
+    for filename in os.listdir(directory):
+        if filename.endswith('.pdf') and filename not in renamed_pdfs:
+            text = extract_text_from_pdf(os.path.join(directory, filename))
+            new_filename_raw = get_filename_from_openai(text)
+
+            # Add condition for same name scenario (prevent duplicate rename and save)
+            if filename == new_filename_raw:
+                print(f"Filename: {filename} already in desired format.")
+                renamed_pdfs.append(new_filename_raw)
+                save_renamed_pdfs(renamed_pdfs)
+                continue
+
+            if new_filename_raw == "ERROR":
+                print("Error processing file: " + filename)
+                save_renamed_pdfs(renamed_pdfs)
+            else:
+                os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename_raw))
+                print("Old Filename: " + filename, "\nNew Filename: " + new_filename_raw)
+                renamed_pdfs.append(new_filename_raw)
+                save_renamed_pdfs(renamed_pdfs)  # Save the renamed files list after every rename
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter, add_help=False)
+    parser.add_argument('-d',
+                        '--dir',
+                        type=str,
+                        default='./data',
+                        help='Directory where the PDFs are stored')
+    parser.add_argument('-h',
+                        '--help',
+                        default=argparse.SUPPRESS,
+                        action='help',
+                        help='usage: python paper_renamer.py -d <directory> or python paper_renamer.py --dir <directory>')
+
+    
+    args = parser.parse_args()
+
+    if args.__contains__('help'):
+        parser.print_help()
+        exit()
+
+
+    main(args.dir)
